baba6a1143a5aa871fc624038662218e159c5670
yassin <hadjsassiyassin@gmail.com>
2025-05-28 14:40:47 +0100

    cors_v7

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -1,16 +1,25 @@
 <?php
-
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\Request;
 
 class Cors
 {
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
+        if ($request->getMethod() === 'OPTIONS') {
+            return response('', 200)
+                ->header('Access-Control-Allow-Origin', '*')
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+                ->header('Access-Control-Allow-Credentials', 'true');
+        }
+
         return $next($request)
-            ->header('Access-Control-Allow-Origin', 'http://localhost:3000') // Replace with your frontend domain
+            ->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+            ->header('Access-Control-Allow-Credentials', 'true');
     }
 }
