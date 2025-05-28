ebab4c16662660e0265ba6c0bc6330cc76f78a2f
hadjsassiyassin <hadjsassiyassin@gmail.com>
2025-05-28 15:53:46 +0000

    v20

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -10,12 +10,12 @@ class Cors
     {
         if ($request->getMethod() === 'OPTIONS') {
             return response('', 200)
- ->header('Access-Control-Allow-Origin', '*')
+ ->header('Access-Control-Allow-Origin', 'https://ehk.vercel.app/')
  ->header('Access-Control-Allow-Methods', '*')
- ->header('Access-Control-Allow-Headers', '*')
+ ->header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization')
  ->header('Access-Control-Allow-Credentials', 'true');
         }
 
-        return $next($request)
+ return $next($request)
     }
 }
